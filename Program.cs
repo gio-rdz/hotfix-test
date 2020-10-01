d0c7b34e3121ae0d0e2ebfbe14e5935845d1875c
Giovanny Rodriguez Aguilar <girodr@microsoft.com>
2020-10-01 15:46:27 -0700

    code change

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace hotfix_test
         static void Main(string[] args)
         {
             Console.WriteLine("New code added");
+            Console.WriteLine("New code added 2");
             Console.WriteLine("Hello World! this is a change");
         }
     }
